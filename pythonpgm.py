133015d0672bd77be534ff0238ab2df180a55c10
Punya-zen <punya.chandrashekar@zentience.co>
2026-02-03 03:09:15 -0800

    Update pythonpgm.py

--- pythonpgm.py
+++ pythonpgm.py
@@ -3,9 +3,7 @@ def add(a, b):
 
 
 # This runs only when you execute this file directly
-if __name__ == "__main__":
-    x = 10
-    y = 20
-    result = add(x, y)
-    print(f"Addition of {x} and {y} is {result}")
+ def test_add() :
+     assert add(1, 2) == 3
+     assert add(1, -1) == 0
 
